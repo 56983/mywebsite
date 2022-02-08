d93864b969c06ff60957a0fb4f56ec3346609e9a
56983 <97937837+56983@users.noreply.github.com>
2022-02-08 15:37:52 +0700

    Update table.php

--- table.php
+++ table.php
@@ -21,6 +21,6 @@
           <td>2</td>
         <td>พัชร์ธีรัตน์ แหลมหลวง</td>
         <td>สไปรท์ บะบะบิ</td>
-        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.komchadluek.net%2Fentertainment%2F490380&psig=AOvVaw3YrPUfXFfqY93J2fzte2Ne&ust=1644395025908000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCRp4rX7_UCFQAAAAAdAAAAABAX
-        <td><a href="https://https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
+        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.komchadluek.net%2Fentertainment%2F490380&psig=AOvVaw3YrPUfXFfqY93J2fzte2Ne&ust=1644395025908000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCRp4rX7_UCFQAAAAAdAAAAABAX.jpeg" width="130px"></td>
+        <td><a href="https://https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor>twitter</a></td>
         <tr>          

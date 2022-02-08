cba41204687c9826c07d4886224e0cd121483614
56983 <97937837+56983@users.noreply.github.com>
2022-02-08 16:26:44 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,12 +15,12 @@
         <td>1</td>
         <td>พัชร์ธีรัตน์ แหลมหลวง</td>
         <td>สไปรท์ บะบะบิ </td>
-        <td><img src="https://sv1.siamnews.com/img_1/13/210923pxo9i.jpg"width="130px"and length="200px"></td>
+        <td><img src="https://sv1.siamnews.com/img_1/13/210923pxo9i.jpg" width="130px"and length="200px"></td>
         <td><a href="https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
        <tr>
           <td>2</td>
         <td>วัชรพล นนท์ภักดี</td>
         <td>ปาร์ตี้</td>
-        <td><img src="https://jelly-site.s3.amazonaws.com/uploads/ckeditor/pictures/30633/content_P1220512-Edit.jpg"width="130px"length="200px"></td>
+        <td><img src="https://jelly-site.s3.amazonaws.com/uploads/ckeditor/pictures/30633/content_P1220512-Edit.jpg" width="130px"length="200px"></td>
         <td><a href="https://twitter.com/papapartyv">twitter</a></td>
          

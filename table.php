0be543c19f93ae7cdfc233c8ae6a4c1f8a965bb6
56983 <97937837+56983@users.noreply.github.com>
2022-02-08 15:49:34 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,14 +13,11 @@
       </tr>
       <tr>
         <td>1</td>
-        <td></td>
-        <td>ขม</td>
-        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
-        <td><a href="https://twitter.com/sjk_kith?lang=en">twitter</a></td>
-       <tr>
-        <td>2</td>
         <td>พัชร์ธีรัตน์ แหลมหลวง</td>
-        <td>สไปรท์ บะบะบิ</td>
-        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.komchadluek.net%2Fentertainment%2F490380&psig=AOvVaw3YrPUfXFfqY93J2fzte2Ne&ust=1644395025908000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCRp4rX7_UCFQAAAAAdAAAAABAX.jpeg" width="130px"></td>
-        <td><a href="https://https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor>twitter</a></td>
-                
+        <td>สไปรท์ บะบะบิ </td>
+        <td><img src="https://sv1.siamnews.com/img_1/13/210923pxo9i.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/SpriteBABABI?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>

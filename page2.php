eabe8ba9e69e0854f1771f7e4566a5267a00d41b
56983 <97937837+56983@users.noreply.github.com>
2022-02-15 14:07:28 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -5,22 +5,17 @@
 			#box1{
 				width: 800px;
 				height: 350px;
+				color : blue
 			}
-			#box2{
-				width: 600px;
-				height: 150px;
-			}
-			.box{
-				background-color: green;
+			body{
+				background-color: pink;
 			}
 		</style>
 	</head>
 	<body>
 		<div id="box1" class="box">
-			<h1>กล่องที่ 1</h1>
-		</div>
-		<div id="box2" class="box">
-			<h1>กล่องที่ 2</h1>
+			<h1>น.ส.กัญญาวีร์ ชัยสิทธิ์ เลขที่ 10 ม.4/13</h1>
+		
 		</div>
 	</body>
 </html>
